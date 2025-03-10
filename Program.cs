aa4c46846f2e01a64ff73794cb3a71fb26080612
iqbalnugrahap <iqbalnugrahapratama123@gmail.com>
2025-03-10 13:31:50 +0700

    first init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args) 
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        for (int i = 4; i >= 0; i--) 
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
